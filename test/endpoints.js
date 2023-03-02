5134c8c8b69523803e9790d232db5db7fd4edf91
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2023-03-02 17:52:12 +0200

    feat: add updateTargetById controller and test

--- test/endpoints.js
+++ test/endpoints.js
@@ -139,3 +139,44 @@ test.serial.cb('get target by id endpoint - first target', function (t) {
     t.end()
   }
 })
+
+test.serial.cb('update target by id endpoint - first target', function (t) {
+  var url = '/api/targets'
+  var options = { encoding: 'json', method: 'POST' }
+  var updatedTarget = {
+    id: 1,
+    url: 'http://example.com',
+    value: '0.50',
+    maxAcceptsPerDay: '17',
+    accept: {
+      geoState: {
+        $in: [
+          'ca',
+          'ny'
+        ]
+      },
+      hour: {
+        $in: [
+          '11',
+          '14',
+          '15'
+        ]
+      }
+    }
+  }
+
+  var expected = {
+    message: 'Target updated successfully'
+  }
+
+  servertest(server(), url, options, onResponse)
+    .end(JSON.stringify(updatedTarget))
+
+  function onResponse (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.deepEqual(res.body, expected, 'values should match')
+    t.end()
+  }
+})
