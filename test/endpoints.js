211a15f6c9f77c407692d4d174714b3a030a908a
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2023-03-02 17:39:27 +0200

    fix: change get target by id route targets->target
    test: add for getTargetById controller

--- test/endpoints.js
+++ test/endpoints.js
@@ -57,40 +57,38 @@ test.serial.cb('create target endpoint - first target', function (t) {
   }
 })
 
-
 test.serial.cb('get all targets endpoint', function (t) {
   var url = '/api/targets'
   var options = { encoding: 'json', method: 'GET' }
 
   var expected = {
-    "message": "Targets retrieved successfully",
-    "response": [
-        {
-            "id": 1,
-            "url": "http://example.com",
-            "value": "0.50",
-            "maxAcceptsPerDay": "10",
-            "accept": {
-                "geoState": {
-                    "$in": [
-                        "ca",
-                        "ny"
-                    ]
-                },
-                "hour": {
-                    "$in": [
-                        "13",
-                        "14",
-                        "15"
-                    ]
-                }
-            }
+    message: 'Targets retrieved successfully',
+    response: [
+      {
+        id: 1,
+        url: 'http://example.com',
+        value: '0.50',
+        maxAcceptsPerDay: '10',
+        accept: {
+          geoState: {
+            $in: [
+              'ca',
+              'ny'
+            ]
+          },
+          hour: {
+            $in: [
+              '13',
+              '14',
+              '15'
+            ]
+          }
         }
+      }
     ]
-}
+  }
 
-  servertest(server(), url, options, onResponse)  
-    
+  servertest(server(), url, options, onResponse)
 
   function onResponse (err, res) {
     t.falsy(err, 'no error')
@@ -101,4 +99,43 @@ test.serial.cb('get all targets endpoint', function (t) {
   }
 })
 
+test.serial.cb('get target by id endpoint - first target', function (t) {
+  var url = '/api/target/1'
+  var options = { encoding: 'json', method: 'GET' }
 
+  var expected = {
+    message: 'Target retrieved successfully',
+    response:
+      {
+        id: 1,
+        url: 'http://example.com',
+        value: '0.50',
+        maxAcceptsPerDay: '10',
+        accept: {
+          geoState: {
+            $in: [
+              'ca',
+              'ny'
+            ]
+          },
+          hour: {
+            $in: [
+              '13',
+              '14',
+              '15'
+            ]
+          }
+        }
+      }
+  }
+
+  servertest(server(), url, options, onResponse)
+
+  function onResponse (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.deepEqual(res.body, expected, 'values should match')
+    t.end()
+  }
+})
