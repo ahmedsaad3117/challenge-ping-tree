d46c01bd626b578c44eb3cfb514229398b283ec1
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2023-03-02 17:04:17 +0200

    feat: add getAllTargets controller and test

--- test/endpoints.js
+++ test/endpoints.js
@@ -42,8 +42,7 @@ test.serial.cb('create target endpoint - first target', function (t) {
   }
 
   var expected = {
-    data: 1,
-    response: 'Target created successfully'
+    message: 'Target created successfully'
   }
 
   servertest(server(), url, options, onResponse)
@@ -57,3 +56,49 @@ test.serial.cb('create target endpoint - first target', function (t) {
     t.end()
   }
 })
+
+
+test.serial.cb('get all targets endpoint', function (t) {
+  var url = '/api/targets'
+  var options = { encoding: 'json', method: 'GET' }
+
+  var expected = {
+    "message": "Targets retrieved successfully",
+    "response": [
+        {
+            "id": 1,
+            "url": "http://example.com",
+            "value": "0.50",
+            "maxAcceptsPerDay": "10",
+            "accept": {
+                "geoState": {
+                    "$in": [
+                        "ca",
+                        "ny"
+                    ]
+                },
+                "hour": {
+                    "$in": [
+                        "13",
+                        "14",
+                        "15"
+                    ]
+                }
+            }
+        }
+    ]
+}
+
+  servertest(server(), url, options, onResponse)  
+    
+
+  function onResponse (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.deepEqual(res.body, expected, 'values should match')
+    t.end()
+  }
+})
+
+
