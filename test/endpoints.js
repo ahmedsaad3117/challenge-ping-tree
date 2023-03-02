9ac597041995c567f9bef132daae58c7ffc43329
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2023-03-02 13:19:29 +0200

    feat: add createTarget controller and test

--- test/endpoints.js
+++ test/endpoints.js
@@ -15,3 +15,45 @@ test.serial.cb('healthcheck', function (t) {
     t.end()
   })
 })
+
+test.serial.cb('create target endpoint - first target', function (t) {
+  var url = '/api/targets'
+  var options = { encoding: 'json', method: 'POST' }
+  var newTarget = {
+    id: 1,
+    url: 'http://example.com',
+    value: '0.50',
+    maxAcceptsPerDay: '10',
+    accept: {
+      geoState: {
+        $in: [
+          'ca',
+          'ny'
+        ]
+      },
+      hour: {
+        $in: [
+          '13',
+          '14',
+          '15'
+        ]
+      }
+    }
+  }
+
+  var expected = {
+    data: 1,
+    response: 'Target created successfully'
+  }
+
+  servertest(server(), url, options, onResponse)
+    .end(JSON.stringify(newTarget))
+
+  function onResponse (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.deepEqual(res.body, expected, 'values should match')
+    t.end()
+  }
+})
