fc8a9b8896ff251e72bfd6a975f30cd5b58a426e
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2023-03-04 19:23:24 +0200

    test: add target created before test

--- test/endpoints.js
+++ test/endpoints.js
@@ -57,6 +57,47 @@ test.serial.cb('create target endpoint - first target', function (t) {
   }
 })
 
+test.serial.cb('create target endpoint - target created before', function (t) {
+  var url = '/api/targets'
+  var options = { encoding: 'json', method: 'POST' }
+  var newTarget = {
+    id: 1,
+    url: 'http://example.com',
+    value: '0.50',
+    maxAcceptsPerDay: '10',
+    accept: {
+      geoState: {
+        $in: [
+          'ca',
+          'ny'
+        ]
+      },
+      hour: {
+        $in: [
+          '13',
+          '14',
+          '15'
+        ]
+      }
+    }
+  }
+
+  var expected = {
+    message: 'Target created before'
+  }
+
+  servertest(server(), url, options, onResponse)
+    .end(JSON.stringify(newTarget))
+
+  function onResponse (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.deepEqual(res.body, expected, 'values should match')
+    t.end()
+  }
+})
+
 test.serial.cb('get all targets endpoint', function (t) {
   var url = '/api/targets'
   var options = { encoding: 'json', method: 'GET' }
