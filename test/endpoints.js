25e080409115a149f83a6a30ac3e253094173d73
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2023-03-05 22:08:29 +0200

    test: add more than one target test

--- test/endpoints.js
+++ test/endpoints.js
@@ -391,6 +391,47 @@ test.serial.cb('makeing decision endpoint - first decision - accept ', function 
   }
 })
 
+test.serial.cb('create target endpoint - second target', function (t) {
+  var url = '/api/targets'
+  var options = { encoding: 'json', method: 'POST' }
+  var newTarget = {
+    id: 1,
+    url: 'http://example.com',
+    value: '0.50',
+    maxAcceptsPerDay: '10',
+    accept: {
+      geoState: {
+        $in: [
+          'ca',
+          'ny'
+        ]
+      },
+      hour: {
+        $in: [
+          '13',
+          '14',
+          '15'
+        ]
+      }
+    }
+  }
+
+  var expected = {
+    message: 'Target created before'
+  }
+
+  servertest(server(), url, options, onResponse)
+    .end(JSON.stringify(newTarget))
+
+  function onResponse (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.deepEqual(res.body, expected, 'values should match')
+    t.end()
+  }
+})
+
 test.serial.cb('makeing decision endpoint - second decision - request more than allows', function (t) {
   var url = '/route'
   var options = { encoding: 'json', method: 'POST' }
